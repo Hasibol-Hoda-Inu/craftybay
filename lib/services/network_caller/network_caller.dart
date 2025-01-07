b297ed4d13812973beb9b4c7a487d25062d7d051
Enu <hasinulhuda@gmail.com>
2025-01-07 18:35:14 +0600

    network caller

--- lib/services/network_caller/network_caller.dart
+++ lib/services/network_caller/network_caller.dart
@@ -36,6 +36,31 @@ class NetworkCaller {
     }
   }
 
+  Future<NetworkResponse> postRequest(String url, {Map<String, dynamic>? body}) async{
+    try{
+      Uri uri = Uri.parse(url);
+      Map<String, String> headers = {
+        "content-type": "application/json",
+      };
+
+      _logRequest(url, headers, body);
+
+      Response response = await post(uri, headers: headers, body: jsonEncode(body),);
+
+      _logResponse(url: url, statusCode: response.statusCode, headers: response.headers, body: response.body);
+
+      final decodedData = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        return NetworkResponse(isSuccess: true, statusCode: response.statusCode, responseData: decodedData);
+      } else {
+        return NetworkResponse(isSuccess: false, statusCode: response.statusCode, errorMessage: decodedData["data"]);
+      }
+    }catch(e){
+      _logResponse(url: url, errorMessage: e.toString());
+      return NetworkResponse(isSuccess: false, statusCode: -1, errorMessage: e.toString());
+    }
+  }
+
   void _logRequest(String url, [Map<String, dynamic>? headers, Map<String, dynamic>? body]){
     _logger.i("URL => $url\nHEADER => $headers\nBODY => $body");
   }
