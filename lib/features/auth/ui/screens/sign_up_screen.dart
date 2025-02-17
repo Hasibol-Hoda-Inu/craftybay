49d5191b96d043e6822b086ee6424c4f0ef502d8
Enu <hasinulhuda@gmail.com>
2025-02-18 00:04:25 +0600

    sign up screen has been updated

--- lib/features/auth/ui/screens/sign_up_screen.dart
+++ lib/features/auth/ui/screens/sign_up_screen.dart
@@ -26,20 +26,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _mobileTEController = TextEditingController();
   final TextEditingController _cityTEController = TextEditingController();
 
-
-  // final TextEditingController _addressTEController = TextEditingController();
-  // final TextEditingController _stateTEController = TextEditingController();
-  // final TextEditingController _postCodeTEController = TextEditingController();
-  // final TextEditingController _countryTEController = TextEditingController();
-  // final TextEditingController _faxTEController = TextEditingController();
-  // final TextEditingController _shippingNameTEController = TextEditingController();
-  // final TextEditingController _shippingAddressTEController = TextEditingController();
-  // final TextEditingController _shippingCityTEController = TextEditingController();
-  // final TextEditingController _shippingStateTEController = TextEditingController();
-  // final TextEditingController _shippingPostCodeTEController = TextEditingController();
-  // final TextEditingController _shippingCountryTEController = TextEditingController();
-  // final TextEditingController _shippingMobileTEController = TextEditingController();
-
   final SignUpController _signUpController = Get.find<SignUpController>();
 
   @override
@@ -53,10 +39,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                const SizedBox(height: 24,),
+                const SizedBox(height: 16,),
                 const AppLogoWidget(),
                 const SizedBox(height: 24,),
-                Text("Complete Profile", style: Theme.of(context).textTheme.titleLarge ,),
+                Text("Sign up", style: Theme.of(context).textTheme.titleLarge ,),
                 Text("Get started with us with your details", style: Theme.of(context).textTheme.bodyLarge,),
                 const SizedBox(height: 24,),
                 Form(
@@ -91,7 +77,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                           ),
                         ),
                         TextFormField(
-                          controller: _cityTEController,
+                          controller: _emailTEController,
                           keyboardType: TextInputType.emailAddress,
                           validator: (value){
                             if(value == null || value.isEmpty){
@@ -146,133 +132,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
                               hintText: 'City'
                           ),
                         ),
-                        // TextFormField(
-                        //   keyboardType: TextInputType.phone,
-                        //   controller: _mobileTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your BD mobile number";
-                        //     }else if(!value.isValidBangladeshiMobile()){
-                        //       return "Enter your mobile number";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Mobile'
-                        //   ),
-                        // ),
-                        // TextFormField(
-                        //   keyboardType: TextInputType.phone,
-                        //   controller: _faxTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your fax number";
-                        //     }else if(!value.isValidBangladeshiMobile()){
-                        //       return "Enter your fax number";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Fax'
-                        //   ),
-                        // ),
-                        // TextFormField(
-                        //   controller: _shippingNameTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your Shipping Name";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Shipping Name'
-                        //   ),
-                        // ),
-                        // TextFormField(
-                        //   controller: _shippingAddressTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your Shipping Address";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Shipping Address'
-                        //   ),
-                        // ),
-                        // TextFormField(
-                        //   controller: _shippingCityTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your Shipping City";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Shipping City'
-                        //   ),
-                        // ),
-                        // TextFormField(
-                        //   controller: _shippingStateTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your Shipping State";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Shipping State'
-                        //   ),
-                        // ),
-                        // TextFormField(
-                        //   controller: _shippingPostCodeTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your Shipping PostCode";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Shipping PostCode'
-                        //   ),
-                        // ),
-                        // TextFormField(
-                        //   controller: _shippingCountryTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your Shipping Country";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Shipping Country'
-                        //   ),
-                        // ),
-                        // TextFormField(
-                        //   keyboardType: TextInputType.phone,
-                        //   controller: _shippingMobileTEController,
-                        //   validator: (value){
-                        //     if(value == null || value.isEmpty){
-                        //       return "Enter your BD shipping phone number";
-                        //     }else if(!value.isValidBangladeshiMobile()){
-                        //       return "Enter your shipping mobile number";
-                        //     }
-                        //     return null;
-                        //   },
-                        //   autovalidateMode: AutovalidateMode.onUserInteraction,
-                        //   decoration: const InputDecoration(
-                        //       hintText: 'Shipping Mobile No.'
-                        //   ),
-                        // ),
-                      ],)),
+                      ],
+                    ),
+                ),
                 const SizedBox(height: 24,),
                 GetBuilder<SignUpController>(
                   builder: (controller) {
@@ -303,28 +165,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   Future<void> _createProfile()async {
     final bool isSuccess = await Get.find<SignUpController>().signUp(
-
-      _firstNameTEController.text.trim(),
+        _firstNameTEController.text.trim(),
         _lastNameTEController.text.trim(),
         _emailTEController.text.trim(),
         _passwordTEController.text,
-      _cityTEController.text.trim(),
-      _mobileTEController.text.trim()
-
-
-        // _nameTEController.text.trim(),
-        // _addressTEController.text.trim(),
-        // _stateTEController.text.trim(),
-        // _postCodeTEController.text.trim(),
-        // _countryTEController.text.trim(),
-        // _faxTEController.text.trim(),
-        // _shippingNameTEController.text.trim(),
-        // _shippingAddressTEController.text.trim(),
-        // _shippingCityTEController.text.trim(),
-        // _shippingStateTEController.text.trim(),
-        // _shippingPostCodeTEController.text.trim(),
-        // _shippingCountryTEController.text.trim(),
-        // _shippingMobileTEController.text.trim(),
+        _cityTEController.text.trim(),
+        _mobileTEController.text.trim()
 
     );
     if(isSuccess){
@@ -354,20 +200,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
     _passwordTEController.dispose();
     _cityTEController.dispose();
     _mobileTEController.dispose();
-
-    // _nameTEController.dispose();
-    // _addressTEController.dispose();
-    // _stateTEController.dispose();
-    // _postCodeTEController.dispose();
-    // _countryTEController.dispose();
-    // _faxTEController.dispose();
-    // _shippingNameTEController.dispose();
-    // _shippingAddressTEController.dispose();
-    // _shippingCityTEController.dispose();
-    // _shippingStateTEController.dispose();
-    // _shippingPostCodeTEController.dispose();
-    // _shippingCountryTEController.dispose();
-    // _shippingMobileTEController.dispose();
     super.dispose();
   }
 }
