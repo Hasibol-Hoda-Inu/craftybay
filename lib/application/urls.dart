b131448801ec4a2d1f24ac72988cf60b64247ff9
Enu <hasinulhuda@gmail.com>
2025-03-01 17:32:37 +0600

    the ecommerce project has been done successfully

--- lib/application/urls.dart
+++ lib/application/urls.dart
@@ -1,6 +1,20 @@
 class Urls{
-  static const String _baseUrl = "https://ecommerce-api.codesilicon.com/api";
+  static const String _baseUrl = "https://ecom-rs8e.onrender.com/api";
 
-  static String verifyEmailUrl(String email) => "$_baseUrl/UserLogin/$email";
-  static String verifyOtpUrl(String email, String otp) => "$_baseUrl/VerifyLogin/$email/$otp";
+  static String signInUrl = "$_baseUrl/auth/login";
+  static String verifyOtpUrl = "$_baseUrl/auth/verify-otp";
+  static String readProfile = "$_baseUrl/ReadProfile";
+  static String signUpUrl = "$_baseUrl/auth/signup";
+  static String homeBannerSliders = "$_baseUrl/slides";
+  static String categoryList = "$_baseUrl/categories";
+  static String productListByCategory(int categoryId) => "$_baseUrl/ListProductByCategory/$categoryId";
+  static String productListByRemarksList = "$_baseUrl/products";
+  static String productDetails= "$_baseUrl/products/id/";
+  static String reviewsUrl = "$_baseUrl/reviews";
+  static String addToCartUrl = "$_baseUrl/cart";
+  static String cartListUrl = "$_baseUrl/cart";
+  static String wishlistUrl = "$_baseUrl/wishlist";
+  static String createOrderUrl = "$_baseUrl/order";
+  static String deleteCartItemUrl(String productId) => "$_baseUrl/cart/$productId";
+  static String deleteWishlistItemUrl(String productId) => "$_baseUrl/wishlist/$productId";
 }
